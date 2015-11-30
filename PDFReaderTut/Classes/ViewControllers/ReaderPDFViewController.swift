26feee3227535c7a3429a93670b6c1b62693fba6
Roman Rybachenko <rom_rybachenko@ukr.net>
2015-11-30 09:29:43 +0200

    created base project

---
+++ PDFReaderTut/Classes/ViewControllers/ReaderPDFViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ReaderPDFViewController.swift
+//  PDFReaderTut
+//
+//  Created by Roman Rybachenko on 11/30/15.
+//  Copyright © 2015 Roman Rybachenko. All rights reserved.
+//
+
+import Cocoa
+
+class ReaderPDFViewController: UIViewController {
+
+}
